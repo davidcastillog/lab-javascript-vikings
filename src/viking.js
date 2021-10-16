946df39c834eef6cc94850ca4592f9de11cf6af9
David Castillo <david@castillog.com>
2021-10-16 16:44:49 -0400

    Done

--- src/viking.js
+++ src/viking.js
@@ -91,10 +91,15 @@ class War {
 }
 
 showStatus(){
-    if (!this.saxonArmy.length) return `Vikings have won the war of the century!`;
-    if (!this.vikingArmy.length) return `Saxons have fought for their lives and survived another day...`;
-    if (this.vikingArmy.length && this.saxonArmy.length) return `Vikings and Saxons are still in the thick of battle.`;
+    if (this.saxonArmy.length === 0) {
+      return `Vikings have won the war of the century!`;
+      } else if (this.vikingArmy.length  === 0) {
+      return `Saxons have fought for their lives and survived another day...`;
+      } else if (this.vikingArmy.length  >= 1 && this.saxonArmy.length >= 1) {
+      return `Vikings and Saxons are still in the thick of battle.`;
+      }
   }
+}
 
 
 
